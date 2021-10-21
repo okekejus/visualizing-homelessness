9d5a5c5cc12753d808e59f82937dbd071b485656
okekejus <91495866+okekejus@users.noreply.github.com>
2021-10-20 22:47:00 -0400

    Create app.R

---
+++ app.R
@@ -0,0 +1,388 @@
+# Homelessness in the World 
+# Author: Justin Okeke 
+# Date: September 20th, 2021
+
+
+library(pacman) # package manager
+
+p_load(leaflet, # map creation
+       tigris, # US Data 
+       rgdal, # Edit shapefiles
+       tidyverse, # data transformation/visualization
+       viridis) # color pallettes 
+
+# Creating the Maps ----------------------------------------------------------------------------------------------------
+
+# United States 
+united.states = states(cb = TRUE)
+
+united.states %>%
+    leaflet() %>%
+    addTiles() %>%
+    addPolygons(popup = ~NAME)
+
+
+# Data ------------------------------------------------------------------------------------------------------------
+
+us.data.2020 <- read_csv("data/New_Data_2020.csv") %>%
+    select(-c(1)) 
+
+
+# Map -------------------------------------------------------------------------------------------------
+
+# United States 
+us.data.2020$State %in% united.states$STUSPS # check the states match
+united.states.merged.home <- geo_join(united.states, us.data.2020, "STUSPS", "State") # combine data and shapefile
+united.states.merged.home <- subset(united.states.merged.home, !is.na(Total.Homeless)) # remove state with no data 
+
+remove(united.states)
+
+
+popup.home <- paste0(united.states.merged.home$NAME, # Popup values when clicked, per state.
+                     "<br> Homelessness Rate: ", 
+                     as.character(round(united.states.merged.home$Homeless.Rate, digits = 0)),
+                     " in 10,000", "<br> Total Homeless: ", 
+                     as.character(united.states.merged.home$Total.Homeless))
+
+us.m <- leaflet(united.states.merged.home) %>%
+    addProviderTiles("CartoDB.Positron") %>%
+    setView(-96, 37.8, 4) # base map
+
+us.m
+
+
+bins <- c(0, 10, 20, 50, 70, 100, 400) # creating intervals for the colors. 
+pal <- colorBin("plasma", domain = united.states.merged.home$Homeless.Rate, bins = bins, pretty = FALSE)
+
+
+total.homeless.map <- us.m %>%
+    addPolygons(fillColor = ~pal(Homeless.Rate), # colored shapes to represent data 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.home,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal, # legend to describe colored shapes. 
+              values = ~Homeless.Rate, 
+              opacity = 0.7,
+              title = "Homeless Rate", 
+              position = "bottomleft")
+
+
+total.homeless.map
+
+
+# Gender  ------------------------------------------------------------------------------------------------------
+summary(united.states.merged.home$Male.Rate)
+bins <- c(0, 5, 10, 15, 25, 50, 100, 150)
+pal <- colorBin("YlOrRd", domain = united.states.merged.home$Male.Rate, bins = bins, pretty = FALSE)
+
+
+popup.male <- paste0(united.states.merged.home$NAME, 
+                     "<br> Homeless, Male: ", 
+                     as.character(round(united.states.merged.home$Male.Rate, digits = 0)),
+                     " in 10,000")
+
+male.homeless.map <- us.m %>%
+    addPolygons(fillColor = ~pal(Male.Rate), 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.male,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal,
+              values = ~Male.Rate, 
+              opacity = 0.7,
+              title = "Rate", 
+              position = "bottomleft")
+male.homeless.map
+
+
+
+# Female 
+pal <- colorBin("YlOrRd", domain = united.states.merged.home$Female.Rate, bins = bins, pretty = FALSE)
+
+popup.female <- paste0(united.states.merged.home$NAME, "<br> Homeless, Female: ", 
+                       as.character(round(united.states.merged.home$Female.Rate, digits = 0)),
+                       " in 10,000")
+
+female.homeless.map <-  us.m %>%
+    addPolygons(fillColor = ~pal(Female.Rate), 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.female,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal,
+              values = ~Female.Rate, 
+              opacity = 0.7,
+              title = "Rate", 
+              position = "bottomleft")
+female.homeless.map
+
+
+
+# Ethnicity Map ---------------------------------------------------------------------------------------------------
+summary(united.states.merged.home$White.Rate)
+
+pal <- colorBin("BuPu", domain = united.states.merged.home$White.Rate, bins = bins, pretty = FALSE)
+
+popup.white <- paste0(united.states.merged.home$NAME, "<br> Homeless, White: ", 
+                      as.character(round(united.states.merged.home$White.Rate, digits = 0)),
+                      " in 10,000")
+
+
+white.homeless.map <- us.m %>%
+    addPolygons(fillColor = ~pal(White.Rate), 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.white,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal,
+              values = ~White.Rate, 
+              opacity = 0.7,
+              title = "Rate", 
+              position = "bottomleft")
+
+white.homeless.map
+
+
+# Black 
+pal <- colorBin("BuPu", domain = united.states.merged.home$Black.Rate, bins = bins, pretty = FALSE)
+popup.black <- paste0(united.states.merged.home$NAME, "<br> Homeless, Black: ", 
+                      as.character(round(united.states.merged.home$Black.Rate, digits = 0)),
+                      " in 10,000")
+
+black.homeless.map <- us.m %>%
+    addPolygons(fillColor = ~pal(Black.Rate), 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.black,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal,
+              values = ~Black.Rate, 
+              opacity = 0.7,
+              title = "Rate", 
+              position = "bottomleft")
+black.homeless.map
+
+
+# Indian 
+summary(united.states.merged.home$Indian.Rate)
+
+bins <- c(0, 3, 5, 7, 12, 15)
+pal <- colorBin("BuPu", domain = united.states.merged.home$Indian.Rate, bins = bins, pretty = FALSE)
+popup.indian <- paste0(united.states.merged.home$NAME, "<br> Homeless, Indian: ", 
+                       as.character(round(united.states.merged.home$Indian.Rate, digits = 0)),
+                       " in 10,000")
+
+indian.homeless.map <- us.m %>%
+    addPolygons(fillColor = ~pal(Indian.Rate), 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.indian,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal,
+              values = ~Indian.Rate, 
+              opacity = 0.7,
+              title = "Rate", 
+              position = "bottomleft")
+indian.homeless.map
+
+
+# Latino 
+summary(united.states.merged.home$Lat.Rate)
+
+pal <- colorBin("BuPu", domain = united.states.merged.home$Lat.Rate, bins = bins, pretty = FALSE)
+popup.lat <- paste0(united.states.merged.home$NAME, "<br> Homeless, Latinx: ", 
+                    as.character(round(united.states.merged.home$Lat.Rate, digits = 0)),
+                    " in 10,000")
+
+lat.homeless.map <- us.m %>%
+    addPolygons(fillColor = ~pal(Lat.Rate), 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.lat,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal,
+              values = ~Lat.Rate, 
+              opacity = 0.7,
+              title = "Rate", 
+              position = "bottomleft")
+lat.homeless.map
+
+
+# Native 
+summary(united.states.merged.home$Native.Rate)
+bins <- c(0, 5, 10, 30, 50, 200)
+
+pal <- colorBin("BuPu", domain = united.states.merged.home$Native.Rate, bins = bins, pretty = FALSE)
+popup.native <- paste0(united.states.merged.home$NAME, "<br> Homeless, Native: ", 
+                       as.character(round(united.states.merged.home$Native.Rate, digits = 0)),
+                       " in 10,000")
+
+native.homeless.map <- us.m %>%
+    addPolygons(fillColor = ~pal(Native.Rate), 
+                weight = 2, 
+                opacity = 1, 
+                color = "white", 
+                dashArray = "2", 
+                fillOpacity = 0.7,
+                smoothFactor = 0.2,
+                popup = ~popup.native,
+                highlightOptions = highlightOptions(
+                    weight = 5, 
+                    color = "grey",
+                    dashArray = "", 
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE)) %>%
+    addLegend(pal = pal,
+              values = ~Native.Rate, 
+              opacity = 0.7,
+              title = "Rate", 
+              position = "bottomleft")
+native.homeless.map
+
+
+
+
+# App -------------------------------------------------------------------------------------------------------------
+
+library(shiny)
+library(shinydashboard)
+
+header <- dashboardHeader(title = "Visualizing Homelessness", titleWidth = 300)
+
+sidebar <- dashboardSidebar(disable = TRUE) # removing the sidebar as I won't really need one.
+
+body <- dashboardBody(
+    fluidRow(
+        column(width = 4, 
+               box(title = "Summary of Total Homelessness", verbatimTextOutput("summary"), width = NULL),
+               valueBoxOutput("total", width = NULL), 
+               box(title = "Total Homelessness, by Region", tableOutput("table"), width = NULL)),
+        column(width = 8,
+               box(title = "Rates of Homelessness, Overall",
+                   leafletOutput("overall"), width = NULL))
+        ), 
+    
+    fluidRow(
+        tabBox(title = "Homelessness by Gender", 
+               selected = "Female",
+               side = "right",
+               tabPanel("Male", leafletOutput("gender.male")), 
+               tabPanel("Female", leafletOutput("gender.female"))), 
+        
+        tabBox(title = "Homelessness by Ethnicity", 
+               selected = "Black", 
+               side = "right", 
+               tabPanel("White", leafletOutput("ethn.white")),
+               tabPanel("Indian", leafletOutput("ethn.indian")), 
+               tabPanel("Latin", leafletOutput("ethn.latin")), 
+               tabPanel("Black", leafletOutput("ethn.black")))
+    )
+)
+
+ui <- dashboardPage(header, sidebar, body)
+
+
+
+
+server <- function(input, output) {
+    # overall maps 
+    output$overall <- renderLeaflet({total.homeless.map})
+    
+    # summary of the total homeless people in the country 
+    output$summary <- renderPrint({
+        summary(united.states.merged.home$Total.Homeless)
+    })
+    
+    output$total <- renderValueBox({
+        valueBox(as.character(sum(united.states.merged.home$Total.Homeless)),
+                 subtitle = paste0("Total Homeless Individuals in the United States"))
+    })
+    
+    # listing gender maps 
+    output$gender.male <- renderLeaflet({male.homeless.map})
+    output$gender.female <- renderLeaflet({female.homeless.map})
+    
+    output$ethn.white <- renderLeaflet({white.homeless.map})
+    output$ethn.asian <- renderLeaflet({indian.homeless.map})
+    output$ethn.latin <- renderLeaflet({lat.homeless.map})
+    output$ethn.black <- renderLeaflet({black.homeless.map})
+    
+    # aggregates of homelessness per region
+    output$table <- renderTable({
+      values <-  aggregate(united.states.merged.home$Total.Homeless, 
+                           by = list(Region = united.states.merged.home$Region), 
+                           FUN = sum)
+      values$x <- round(values$x, digits = 0)
+      values <- values %>%
+        rename(`Homeless Population` = x)
+      
+      values
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
